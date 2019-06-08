d477cb5378f4a4341eb89aa94716d0333086b438
Norman Desch <normandesch3@gmaill.com>
2019-06-07 22:35:47 -0400

    rule created

--- src/db/migrations/20190608015019-create-rule.js
+++ src/db/migrations/20190608015019-create-rule.js
@@ -1,36 +1,22 @@
 'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Rules', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      topicId: {
-        type: Sequelize.INTEGER,
-        onDelete: "CASCADE",
-        references: {
-          model: "Topics",
-          key: "id",
-          as: "topicId",
-        },
-      }
+module.exports = (sequelize, DataTypes) => {
+  const Topic = sequelize.define('Topic', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  Topic.associate = function(models) {
+    // associations can be defined here
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId",
+      as: "banners",
     });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Rules');
-  }
+
+  };
+return Topic;
 };
--- src/db/models/rule.js
+++ src/db/models/rule.js
@@ -1,21 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Rule = sequelize.define('Rule', {
-    description: DataTypes.STRING,
-   topicId: {
-     type: DataTypes.INTEGER,
-     onDelete: "CASCADE",
-     references: {
-       model: "Topic",
-       key: "id",
-       as: "topicId",
-     }
-   }
+    description: DataTypes.STRING
   }, {});
   Rule.associate = function(models) {
-    Rule.belongsTo(models.Topic,{
+    Rule.belongsTo(models.Topic, {
       foreignKey: "topicId",
-      onDelete: "CASCADE",
+        onDelete: "CASCADE"
     });
   };
   return Rule;
