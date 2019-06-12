87471b32dc966226a903e46a621f8395f924b80f
Norman Desch <normandesch3@gmaill.com>
2019-06-12 00:21:36 -0400

    validations done

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,6 +2,7 @@ const staticRoutes = require("../routes/static");
 const topicRoutes = require("../routes/topics");
 const advertisementRoutes = require("../routes/advertisements");
 const postRoutes = require ("../routes/posts");
+const userRoutes = require("../routes/users");
 const flairRoutes = require ("../routes/flairs");
 
 module.exports = {
@@ -10,6 +11,7 @@ module.exports = {
     app.use(topicRoutes);
     app.use(advertisementRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
     app.use(flairRoutes);
   }
 }
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -19,6 +19,25 @@ module.exports = {
        }
    },
 
+   validateUsers(req, res, next) {
+     if(req.method === "POST") {
+
+// #1
+       req.checkBody("email", "must be valid").isEmail();
+       req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+       req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+     }
+
+     const errors = req.validationErrors();
+
+     if (errors) {
+       req.flash("error", errors);
+       return res.redirect(req.headers.referer);
+     } else {
+       return next();
+     }
+   },
+
    validateTopics(req, res, next){
        if(req.method === "POST") {
            req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
@@ -33,4 +52,5 @@ module.exports = {
       return next();
     }
   }
+
 }
